14195dd325d523b311e394b36935210dd2f56d41
Paulo Ricardo <pricardo.ti@gmail.com>
2024-05-28 15:15:40 -0300

    feat: versao entrega do projeto

--- src/main/java/br/com/delivery/delivery/application/ports/inbound/pedido/EditarPedidoInboundPort.java
+++ src/main/java/br/com/delivery/delivery/application/ports/inbound/pedido/EditarPedidoInboundPort.java
@@ -1,8 +1,9 @@
 package br.com.delivery.delivery.application.ports.inbound.pedido;
 
+import br.com.delivery.delivery.application.domain.enums.StatusPedido;
 import br.com.delivery.delivery.application.domain.pedido.Pedido;
 
 public interface EditarPedidoInboundPort {
 
-    void editar(Pedido pedido);
+    void editar(Pedido pedido, StatusPedido statusPedido);
 }
--- src/main/java/br/com/delivery/delivery/application/usecases/pedido/EditarPedidoInboundUseCase.java
+++ src/main/java/br/com/delivery/delivery/application/usecases/pedido/EditarPedidoInboundUseCase.java
@@ -1,9 +1,12 @@
 package br.com.delivery.delivery.application.usecases.pedido;
 
+import br.com.delivery.delivery.application.domain.enums.StatusPedido;
 import br.com.delivery.delivery.application.domain.pedido.Pedido;
 import br.com.delivery.delivery.application.ports.inbound.pedido.EditarPedidoInboundPort;
 import br.com.delivery.delivery.application.ports.outbound.pedido.EditarPedidoOutboundPort;
 import lombok.RequiredArgsConstructor;
+
+import static br.com.delivery.delivery.application.domain.enums.StatusPedido.AGUARDANDO_PAGAMENTO;
 
 @RequiredArgsConstructor
 public class EditarPedidoInboundUseCase implements EditarPedidoInboundPort {
@@ -11,7 +14,11 @@ public class EditarPedidoInboundUseCase implements EditarPedidoInboundPort {
     private final EditarPedidoOutboundPort editarPedidoOutboundPort;
 
     @Override
-    public void editar(Pedido pedido) {
+    public void editar(Pedido pedido, StatusPedido statusPedido) {
+        if (AGUARDANDO_PAGAMENTO.equals(pedido.getStatus()))
+            throw new IllegalArgumentException("o status do pedido nao pode ser atualizado, por favor realizar o checkout.");
+
+        pedido.setStatus(statusPedido);
         editarPedidoOutboundPort.editar(pedido);
     }
 }
