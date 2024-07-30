47e1ab462d04e6cb74ff984b22eb80a3590e39c6
Paulo Ricardo <pricardo.ti@gmail.com>
2024-07-30 19:16:21 -0300

    doc: atualizando descrição da arquitetura

--- src/main/java/br/com/fiap/delivery/application/presenters/cliente/CadastrarClientePresenter.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.delivery.application.presenters.cliente;
-
-import br.com.fiap.delivery.domain.cliente.Cliente;
-
-public interface CadastrarClientePresenter<T> {
-
-    T handler(Cliente cliente);
-}
